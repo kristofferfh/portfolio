a050ad615e8f2a750a4e68d8b16d91df92f7d9c0
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-08-14 17:56:30 +0200

    style tweaks and type errors

--- components/elements/footer/footer.tsx
+++ components/elements/footer/footer.tsx
@@ -10,11 +10,11 @@ const FooterEl = () => {
  return (
   <footer className={styles.footer} style={{backgroundImage: `url(${waves.src})`}}>
    <div className={styles.socials}>
-    <a href="https://twitter.com/username" target="_blank"><Image src={socialIcons.twitter.src} alt="link to my twitter" /></a>
-    <a href="https://stackoverflow.com/users/0" target="_blank"><Image src={socialIcons.stackoverflow.src} alt="link to my stackoverflow" /></a>
-    <a href="https://github.com/username" target="_blank"><Image src={socialIcons.github.src} alt="link to my github" /></a>
-    <a href="https://www.youtube.com/usrnme" target="_blank"><Image src={socialIcons.youtube.src} alt="link to my youtube" /></a>
-    <a href="https://discord.com/user/username" target="_blank"><Image src={socialIcons.discord.src} alt="link to my discord server" /></a>
+    <a href="https://twitter.com/username" target="_blank"><Image src={socialIcons.twitter.src} alt="link to my twitter" height={48} width={48} /></a>
+    <a href="https://stackoverflow.com/users/0" target="_blank"><Image src={socialIcons.stackoverflow.src} alt="link to my stackoverflow" height={48} width={48} /></a>
+    <a href="https://github.com/username" target="_blank"><Image src={socialIcons.github.src} alt="link to my github" height={48} width={48} /></a>
+    <a href="https://www.youtube.com/usrnme" target="_blank"><Image src={socialIcons.youtube.src} alt="link to my youtube" height={48} width={48} /></a>
+    <a href="https://discord.com/user/username" target="_blank"><Image src={socialIcons.discord.src} alt="link to my discord server" height={48} width={48} /></a>
    </div>
    {/* <div className={styles.section}>
     <div className={styles.about}>
--- components/projectcard/projectcard.tsx
+++ components/projectcard/projectcard.tsx
@@ -26,10 +26,9 @@ export const ProjectCard = ({props}:ProjectCardProps) => {
  return (
   <motion.div 
   className={styles.projectcard}
-  
   >
     <h2>{props.title}</h2>
-    <Image src={`blog/${props.img}`} alt="" />
+    <Image src={`/blog/${props.img}`} alt="" height={500} width={500} />
     <p>{props.desc.short}</p>
     <span className={styles.tags}>{props.tags.map((tag)=>(<a key={tag}>{tag}</a>))}</span>
   </motion.div>
