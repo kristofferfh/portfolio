2ae0bbcc7700d6abb471b977e3f94076d0c9a725
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-04 16:31:27 +0200

    Creating header and footer base component

---
+++ components/footer/Index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer>
+    <div>
+
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/header/Index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <!-- <NuxtLink href="/">Home</NuxtLink> -->
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
