011298feb11eaf55d74811f459078b927a8ffbcc
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-07 14:25:25 +0200

    Creating nav menu inside header

---
+++ components/header/Nav.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  // Get menu items from nuxt.config, tmp while dev
+  const menuItems = [
+    { name: "Home", path: "/" },
+    { name: "Timeline", path: "/timeline" },
+    { name: "Projects", path: "/projects" },
+    { name: "Contact", path: "/contact" },
+  ]
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li v-for="item in menuItems">
+        <a :href="item.path">{{ item.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+  ul {
+    display: flex;
+    gap: 2rem;
+    list-style: none;
+  }
+  li {
+    padding: 2rem;
+  }
+</style>
