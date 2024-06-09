b0b7fd009af77de82aa74f4486a83fdd42afebec
Kristoffer <kristofferfh.kodehode@gmail.com>
2024-06-09 18:30:36 +0200

    Button now jas a state to reflect action

--- components/header/Button.vue
+++ components/header/Button.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-// Create svg hamburger menu where the shape changes on hover
-// Perhaps an icon that changes into a circle that then changes into rect with menu inside?
+  // Create svg hamburger menu where the shape changes on hover
+  // Perhaps an icon that changes into a circle that then changes into rect with menu inside?
+
+  const props = defineProps(['isOpen'])
 </script>
 
 <template>
   <span id="header-button">
-    |||
+    {{ props.isOpen ? 'close' : 'open' }}
   </span>
 </template>
 
