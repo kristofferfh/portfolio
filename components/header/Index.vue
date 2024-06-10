4956010493b085a92615a3655d9bb83943f493e6
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-10 17:03:42 +0200

    Added transition and timing functions for smoother animation

--- components/header/Index.vue
+++ components/header/Index.vue
@@ -1,23 +1,52 @@
 <script setup lang="ts">
-  const isOpen = ref(true)
+  // Convert css transition to func instead or use media queuery to find size of
+  // nav el to scale border-radius better.
 
-  // The plan here is to have the header element expand rtl on hover with an effect that exands slowly
-  // Another important thing is to make sure the border goes from buttons circle shape to nav bars rect shape
+  const isOpen = ref(true)
 </script>
 
 <template>
-  <header :class="{'nav-open': isOpen}">
-    <Transition :name="isOpen ? 'btn' : 'nav'" mode="out-in">
-      <HeaderNav v-if="isOpen" @mouseleave="isOpen = false"/>
-      <HeaderButton v-else @mouseenter="isOpen = true" />
+  <header
+    @mouseenter="isOpen = true"
+    @mouseleave="isOpen = false"
+    :class="{'opened': isOpen}"
+  >
+    <Transition>
+      <HeaderNav v-if="isOpen" />
     </Transition>
+    <HeaderButton @click="isOpen = !isOpen" :isOpen="isOpen"/>
   </header>
 </template>
 
 <style scoped>
   header {
+    display: flex;
+    justify-content: end;
     position: fixed;
     top: 2rem;
     right: 2rem;
+
+    color: var(--txt-header);
+    background-color: var(--bg-primary);
+    border: 2px solid #4d4d4d;
+    border-radius: 50%;
+
+    transition: border-radius 1200ms cubic-bezier(1,0,.9,.3);
+  }
+
+  .opened {
+    transition: border-radius 100ms ease-out;
+    border-radius: 10px;
+  }
+
+  /* we will explain what these classes do next! */
+  .v-enter-active,
+  .v-leave-active {
+    transition: all 1s ease-in-out;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    max-width: 0;
   }
 </style>
