a31034c1ab82a5ac73ef1c32ae0dbac594bbb193
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-10 17:03:54 +0200

    Better list styling and colors

--- components/header/Nav.vue
+++ components/header/Nav.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   // Get menu items from nuxt.config, tmp while dev
+
   const menuItems = [
     { name: "Home", path: "/" },
     { name: "Timeline", path: "/timeline" },
@@ -12,17 +13,45 @@
   <nav>
     <ul>
       <li v-for="item in menuItems">
-        <NuxtLink :href="item.path">{{ item.name }}</NuxtLink>
+        <NuxtLink :to="item.path">{{ item.name }}</NuxtLink>
       </li>
     </ul>
   </nav>
 </template>
 
 <style scoped>
+  nav {
+    overflow: hidden;
+    max-width: 1000px;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  li {
+    display: flex;
+  }
+  li:first-child {
+    border-radius: 10px 0 0 10px;
+  }
+  li:hover {
+    background-color: #2d2d2d;
+  }
+  li:active {
+    background-color: #3d3d3d;
+  }
+
   a {
-    padding: 2rem 4rem;
+    padding: 1rem 3rem;
     font-family: 'Nunito', Arial, Helvetica, sans-serif;
-    color: var(--color-txt-light);
+    color: var(--txt-header);
     text-decoration: none;
+    font-size: large;
+  }
+
+  .router-link-active {
+    color: orange;
   }
 </style>
