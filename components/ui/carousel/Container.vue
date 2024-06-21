e44c758d71015fb9597781f7e7706d00485285cc
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-21 15:52:52 +0200

    Splitting component into children and moving into UI folder

---
+++ components/ui/carousel/Container.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  const props = defineProps<Props>()
+
+  interface Image {
+    name: string
+    src: string
+    desc: string
+  }
+
+  interface Props {
+    images: Image[]
+  }
+</script>
+
+<template>
+  <div>
+    <LazyUiCarouselImage v-for="item in images" v-bind="item" />
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    position: relative;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    background-color: orange;
+  }
+</style>
---
+++ components/ui/carousel/Image.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+  const props = defineProps<Image>()
+
+  interface Image {
+    name: string
+    src: string
+    desc: string
+  }
+
+  // pass translateX and rotate?
+</script>
+
+<template>
+  <span>
+    <LazyNuxtImg :src="`projects/${props.src}`"/>
+    <h3>{{ props.name }}</h3>
+    <p>{{ props.desc }}</p>
+  </span>
+</template>
+
+<style scoped>
+  span {
+    position: absolute;
+    overflow: visible;
+    width: clamp(500px, 100%, 1024px);
+    height: 100%
+  }
+
+  img {
+    position: absolute;
+    width: 100%;
+    min-height: 100%;
+    object-fit: cover;
+  }
+
+  h3 {
+    position: absolute;
+    padding: 1rem;
+    top: 30%;
+    transform: translateY(-50%);
+    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+  }
+
+  p {
+    position: absolute;
+    bottom: 0;
+    padding: 1rem;
+  }
+</style>
