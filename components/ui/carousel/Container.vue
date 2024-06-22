149f0475f8065d546a302e53f8a67c5cdbc90560
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-22 18:50:38 +0200

    Adding pagination to image carousel

--- components/ui/carousel/Container.vue
+++ components/ui/carousel/Container.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   const props = defineProps<Props>()
+  const activePage = ref(0)
+  const currentPages = ref()
 
   interface Image {
     name: string
@@ -11,11 +13,24 @@
     images: Image[]
     active: boolean
   }
+
+  onMounted(() => {
+    currentPages.value = props.images.length - 1
+  })
+
+  const pageinate = (newPage:number) => {
+    if (newPage < 0 ) activePage.value = currentPages.value
+    if (newPage > currentPages.value) activePage.value = 0
+  }
 </script>
 
 <template>
   <div>
-    <LazyUiCarouselImage v-for="item in props.images" v-bind="item" :state="props.active" />
+    <template v-for="(item, index) in props.images">
+      <LazyUiCarouselImage v-bind="item" :state="props.active" :active="index === activePage" />
+    </template>
+    <button @click="pageinate(--activePage)">left </button>
+    <button @click="pageinate(++activePage)">{{ activePage }}</button>
   </div>
 </template>
 
@@ -27,5 +42,15 @@
     width: 100%;
     height: 100%;
     background-color: orange;
+  }
+
+  button {
+    position: absolute;
+    width: 20%;
+    height: 100%;
+  }
+
+  button:last-of-type {
+    right: 0;
   }
 </style>
